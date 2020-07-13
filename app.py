f1c65b43fade8bf788cf1c97e2ff10eb756fec2d
ashish-pondit <48186079+ashish-pondit@users.noreply.github.com>
2020-07-13 23:54:47 +0600

    Update app.py
    printng statement

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def index():
         
         #print('yes it happend but why')
         model=pickle.load(open('model.pkl','rb'))
+        print('printing model')
         print(model)
         values = np.array([w,x,y,z]).reshape(1,4)
         #pred = model.predict(values)
